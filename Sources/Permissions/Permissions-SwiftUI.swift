94f9e6a9dd98e19b476a973e18dbf287a7f62882
timi2506 <172171055+timi2506@users.noreply.github.com>
2025-10-23 15:13:06 +0200

    A

---
+++ Sources/Permissions/Permissions-SwiftUI.swift
@@ -0,0 +1,307 @@
+import SwiftUI
+
+public extension View {
+    func permissionsSheet(isPresented: Binding<Bool>, permissions: Set<PermissionType>) -> some View {
+        self.modifier(PermissionsSheet(isPresented: isPresented, perms: permissions.map { $0 }))
+    }
+}
+
+private struct PermissionsSheet: ViewModifier {
+    @StateObject var manager = PermissionsManager.shared
+    @Binding var isPresented: Bool
+    let perms: Array<PermissionType>
+    @State var currentIndex: Int = 0
+    @State var hasFinished = false
+    @State var isRefreshing = false
+    private var currentPermission: PermissionType? {
+        guard perms.indices.contains(currentIndex) else { return nil }
+        return perms[currentIndex]
+    }
+    func body(content: Content) -> some View {
+        content
+            .sheet(isPresented: $isPresented) {
+                if isRefreshing {
+                    ProgressView()
+                } else {
+                    sheetView
+                }
+            }
+    }
+    var sheetView: some View {
+        NavigationView {
+            VStack {
+                if let permission = currentPermission {
+                    PermissionView(permission: permission) {
+                        withAnimation {
+                            isRefreshing = true
+                            if currentIndex + 1 < perms.count {
+                                currentIndex += 1
+                            } else if currentIndex + 1 == perms.count {
+                                // ALL PERMS FINISHED
+                                hasFinished = true
+                                currentIndex += 1
+                            }
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                                withAnimation {
+                                    isRefreshing = false
+                                }
+                            }
+                        }
+                    }
+                    .animation(.default, value: currentIndex)
+                } else {
+                    if hasFinished {
+                        finishedView
+                    } else {
+                        Text("An Error occured")
+                        Button("Dismiss") {
+                            isPresented = false
+                        }
+                    }
+                }
+            }
+            .animation(.default, value: currentIndex)
+            .navigationTitle(manager.localizationProvider.permissionsTitle)
+            .navigationBarTitleDisplayMode(.inline)
+        }
+    }
+    var finishedView: some View {
+        VStack {
+            VStack {
+                Image(systemName: "checkmark.circle.fill")
+                    .font(.system(size: 75))
+                    .frame(height: 100)
+                    .padding()
+                Text(manager.localizationProvider.allSetTitle)
+                    .font(.title)
+                    .bold()
+                Text(manager.localizationProvider.allSetDescription)
+                    .foregroundStyle(.secondary)
+            }
+            .multilineTextAlignment(.center)
+            .padding(.vertical, 50)
+            .frame(maxWidth: .infinity)
+            .contentShape(.rect)
+            Spacer()
+            Button(action: {
+                
+            }) {
+                HStack {
+                    Spacer()
+                    Text(manager.localizationProvider.doneButtonTitle)
+                        .bold()
+                    Spacer()
+                }
+                .padding(10)
+            }
+            .borderedProminent()
+            .padding()
+            .padding(.horizontal)
+        }
+    }
+}
+
+private struct PermissionView: View {
+    @StateObject var manager = PermissionsManager.shared
+    @State var isGranted = false
+    @State var isProcessing = false
+    @State var isAnimating = false
+    @State var showSkip = false
+    @State var showHelp = false
+    var permission: PermissionType
+    var next: () -> Void
+    var body: some View {
+        VStack {
+            header()
+                .onTapGesture {
+                    showHelp.toggle()
+                }
+            Spacer()
+            ZStack {
+                VStack {
+                    Text(manager.localizationProvider.skipNoticeText)
+                        .font(.caption)
+                        .foregroundStyle(.secondary)
+                        .multilineTextAlignment(.center)
+                    Button(manager.localizationProvider.tapHereToSkipButtonTitle) {
+                        next()
+                    }
+                    .font(.caption)
+                    .multilineTextAlignment(.center)
+                }
+                .offset(y: showSkip ? -60 : 0)
+                .padding(.horizontal, 50)
+                Button(action: {
+                    Task {
+                        if !isGranted {
+                            await request()
+                        } else {
+                            next()
+                        }
+                    }
+                }) {
+                    HStack {
+                        Spacer()
+                        if isProcessing {
+                            ProgressView()
+                        }
+                        Text(isProcessing ? manager.localizationProvider.waitingTitle : isGranted ? manager.localizationProvider.continueButtonTitle : manager.localizationProvider.grantButtonTitle)
+                            .bold()
+                            .contentTransition()
+                        Spacer()
+                    }
+                    .padding(10)
+                }
+                .borderedProminent()
+                .disabled(isProcessing)
+                .padding()
+                .padding(.horizontal)
+            }
+        }
+        .helpOverlayView(isPresented: $showHelp) {
+            NavigationView {
+                Form {
+                    header(false)
+                    Section(manager.localizationProvider.whatDoesThisPermissionDoSectionTitle) {
+                        Text(permission.localizedDesciption)
+                    }
+                }
+                .navigationTitle(manager.localizationProvider.helpNavigationTitle)
+                .navigationBarTitleDisplayMode(.inline)
+                .toolbar {
+                    ToolbarItem(placement: .cancellationAction) {
+                        if #available(iOS 26, *) {
+                            Button("Close", systemImage: "xmark") {
+                                showHelp = false
+                            }
+                        } else {
+                            Button(action: {
+                                showHelp = false
+                            }) {
+                                Image(systemName: "xmark")
+                                    .font(.system(size: 15, weight: .bold))
+                                    .foregroundStyle(.secondary)
+                                    .padding(7.5)
+                                    .background(
+                                        Circle()
+                                            .foregroundStyle(.tertiary)
+                                    )
+                            }
+                        }
+                    }
+                }
+            }
+            .navigationViewStyle(.stack)
+        }
+        .task {
+            isGranted = await permission.isAuthorized()
+        }
+    }
+    func changeProcessing(to newState: Bool) async {
+        withAnimation {
+            isAnimating = true
+        }
+        await wait(for: 0.25)
+        withAnimation {
+            isProcessing = newState
+        }
+        await wait(for: 0.25)
+        withAnimation {
+            isAnimating = false
+        }
+    }
+    func checkGranted(disableRepeatedAnimation: Bool = false) async {
+        if !disableRepeatedAnimation {
+            await changeProcessing(to: true)
+        }
+        isGranted = await permission.isAuthorized()
+        withAnimation(.bouncy) {
+            showSkip = true
+        }
+        if !disableRepeatedAnimation {
+            await changeProcessing(to: false)
+        }
+    }
+    func request() async {
+        await changeProcessing(to: true)
+        await permission.request()
+        await checkGranted(disableRepeatedAnimation: true)
+        await changeProcessing(to: false)
+    }
+    func header(_ showGranted: Bool = true) -> some View {
+        VStack(spacing: 10) {
+            Image(systemName: permission.sfSymbol)
+                .font(.system(size: 75))
+                .frame(height: 100)
+                .padding()
+            Text(manager.localizationProvider.sheetHeader)
+                .foregroundStyle(.secondary)
+            Text(permission.localizedTitle)
+                .font(.title)
+                .bold()
+            if showGranted {
+                HStack {
+                    Image(systemName: isGranted ? "checkmark.circle.fill" : "xmark.circle.fill")
+                        .font(.system(size: 17.5))
+                    
+                    Text(isGranted ? manager.localizationProvider.grantedTitle : manager.localizationProvider.notGrantedTitle)
+                        .bold()
+                        .font(.system(size: 20))
+                }
+                .foregroundStyle(isGranted ? .green : .red)
+            }
+        }
+        .multilineTextAlignment(.center)
+        .padding(.vertical, 50)
+        .frame(maxWidth: .infinity)
+        .contentShape(.rect)
+    }
+}
+
+fileprivate func wait(for seconds: TimeInterval) async {
+    try? await Task.sleep(nanoseconds: .seconds(seconds))
+}
+
+fileprivate extension UInt64 {
+    static func seconds(_ value: TimeInterval) -> UInt64 {
+        return UInt64(value * 1_000_000_000)
+    }
+}
+
+fileprivate extension View {
+    func borderedProminent() -> some View {
+        if #available(iOS 26.0, *) {
+            return self
+                .buttonStyle(.glassProminent)
+        } else {
+            return self
+                .buttonStyle(.borderedProminent)
+        }
+    }
+    func contentTransition() -> some View {
+        if #available(iOS 16.0, *) {
+            return self                       .contentTransition(.numericText(countsDown: false))
+        } else {
+            return self
+        }
+    }
+    func helpOverlayView<Content: View>(isPresented: Binding<Bool>, content: @escaping () -> Content) -> some View {
+        if #available(iOS 16.0, *) {
+            return self
+                .sheet(isPresented: isPresented) {
+                    content()
+                        .presentationDetents([.large, .medium])
+                }
+        } else {
+            return self
+                .fullScreenCover(isPresented: isPresented) {
+                    content()
+                }
+        }
+    }
+}
+
+#Preview {
+    Text("Hello World!")
+        .permissionsSheet(isPresented: .constant(true), permissions: [.camera, .microphone, .bluetooth])
+}
---
+++ Sources/Permissions/Permissions.swift
@@ -0,0 +1,359 @@
+import SwiftUI
+import CoreBluetooth
+import Contacts
+import AVFoundation
+import CoreLocation
+import UserNotifications
+import Photos
+import Combine
+
+public protocol PermissionsLocalizationProvider {
+    /// A Short Title for the Permission, e.g. "Microphone"
+    func makeLocalizedTitle(for type: PermissionType) -> LocalizedStringKey
+    /// A Descriptive Text explaining the user what the given Permission grants your App access to
+    func makeLocalizedDescription(for type: PermissionType) -> LocalizedStringKey
+    /// The header to display on top of Permissions in the Permission Sheet, Default: "This App Requires Access to"
+    var sheetHeader: LocalizedStringKey { get set }
+    /// The Title to display when setup has completed in the Permission Sheet, Default: "All Set!"
+    var allSetTitle: LocalizedStringKey { get set }
+    /// The Description to display when setup has completed in the Permission Sheet, Default: "Tap Done below to start using this App"
+    var allSetDescription: LocalizedStringKey { get set }
+    /// The Text to Display when a Permission is granted
+    var grantedTitle: LocalizedStringKey { get set }
+    /// The Text to Display when a Permission is not granted
+    var notGrantedTitle: LocalizedStringKey { get set }
+    /// The Grant Buttons Title
+    var grantButtonTitle: LocalizedStringKey { get set }
+    /// The Continue Buttons Title
+    var continueButtonTitle: LocalizedStringKey { get set }
+    /// The Done Buttons Title
+    var doneButtonTitle: LocalizedStringKey { get set }
+    /// The Text to display while waiting
+    var waitingTitle: LocalizedStringKey { get set }
+    /// The "Tap Here to Skip" Buttons Title, its displayed below the skipNoticeText
+    var tapHereToSkipButtonTitle: LocalizedStringKey { get set }
+    /// The Skip Notice, by default: "If you do not want to grant this Permission, you can"
+    var skipNoticeText: LocalizedStringKey { get set }
+    /// The "Help" Sheet Title Header
+    var helpNavigationTitle: LocalizedStringKey { get set }
+    /// The "What does this Permission do?" Title of the Section in the Help Sheet
+    var whatDoesThisPermissionDoSectionTitle: LocalizedStringKey { get set }
+    /// The "Permissions" Title shown in the Sheet
+    var permissionsTitle: LocalizedStringKey { get set }
+}
+
+public extension PermissionsLocalizationProvider where Self == DefaultPermissionsLocalizationProvider {
+    static var defaultProvider: PermissionsLocalizationProvider { DefaultPermissionsLocalizationProvider() }
+}
+
+public struct DefaultPermissionsLocalizationProvider: PermissionsLocalizationProvider {
+    public var permissionsTitle: LocalizedStringKey = "Permissions"
+    
+    public var whatDoesThisPermissionDoSectionTitle: LocalizedStringKey = "What does this Permission do?"
+    
+    public var helpNavigationTitle: LocalizedStringKey = "Help"
+    
+    public var tapHereToSkipButtonTitle: LocalizedStringKey = "Tap here to Skip"
+    
+    public var skipNoticeText: LocalizedStringKey = "If you do not want to grant this Permission, you can"
+    
+    public var grantButtonTitle: LocalizedStringKey = "Grant"
+    
+    public var continueButtonTitle: LocalizedStringKey = "Continue"
+    
+    public var doneButtonTitle: LocalizedStringKey = "Done"
+    
+    public var waitingTitle: LocalizedStringKey = "Waiting"
+    
+    public var grantedTitle: LocalizedStringKey = "Granted"
+    
+    public var notGrantedTitle: LocalizedStringKey = "Not Granted"
+    
+    public var allSetDescription: LocalizedStringKey = "Tap Done below to start using this App"
+    public var allSetTitle: LocalizedStringKey = "All Set!"
+    
+    public func makeLocalizedTitle(for type: PermissionType) -> LocalizedStringKey {
+        return switch type {
+            case .camera:
+                "Camera"
+            case .microphone:
+                "Microphone"
+            case .locationWhenInUse:
+                "Location while in Use"
+            case .locationAlways:
+                "Location at all Times"
+            case .notifications:
+                "Notifications"
+            case .photoLibrary:
+                "Photo Library"
+            case .contacts:
+                "Contacts"
+            case .bluetooth:
+                "Bluetooth"
+        }
+    }
+    public func makeLocalizedDescription(for type: PermissionType) -> LocalizedStringKey {
+        return switch type {
+            case .camera:
+                "The \"Camera\" permission allows this app to access your camera to take photos or record videos.\n\nExample: A camera app that lets you capture photos and videos directly within the app."
+            case .microphone:
+                "The \"Microphone\" permission allows this app to access your microphone to record audio.\n\nExample: A voice memos app that lets you record audio notes."
+            case .locationWhenInUse:
+                "The \"Location While in Use\" permission allows this app to access your location only while you are actively using it.\n\nExample: A weather app that shows your current weather based on your precise location while the app is open."
+            case .locationAlways:
+                "The \"Location Always\" permission allows this app to access your location even when it is running in the background.\n\nExample: A navigation app that provides turn-by-turn directions even when you switch to another app."
+            case .notifications:
+                "The \"Notifications\" permission allows this app to send you alerts and updates.\n\nExample: A messaging app that notifies you of new messages in your chats."
+            case .photoLibrary:
+                "The \"Photo Library\" permission allows this app to access and modify your photos.\n\nExample: A photo viewer app that lets you browse, edit, and organize your photos."
+            case .contacts:
+                "The \"Contacts\" permission allows this app to access your contacts.\n\nExample: A messaging app that helps you find friends and connect with people in your contact list."
+            case .bluetooth:
+                "The \"Bluetooth\" permission allows this app to connect to nearby devices using Bluetooth.\n\nExample: A fitness app that syncs data with a Bluetooth heart rate monitor."
+        }
+    }
+    public var sheetHeader: LocalizedStringKey = "This App Requires Access to"
+}
+
+import os
+
+@MainActor
+public enum PermissionType: String, CaseIterable, RawRepresentable {
+    case camera, microphone, locationWhenInUse, locationAlways, notifications, photoLibrary, contacts, bluetooth
+    
+    /// The Localization Provider used to Localize the Permissions' Titles and Descriptions, you can set this to a Custom Provider by Changing PermissionsManager.shared.localizationProvider
+    static var localizationProvider: PermissionsLocalizationProvider {
+        PermissionsManager.shared.localizationProvider
+    }
+    
+    /// A Short Title for the Permission, e.g. "Microphone"
+    var localizedTitle: LocalizedStringKey {
+        return Self.localizationProvider.makeLocalizedTitle(for: self)
+    }
+    
+    /// A Descriptive Text explaining the user what the given Permission grants your App access to
+    var localizedDesciption: LocalizedStringKey {
+        return Self.localizationProvider.makeLocalizedDescription(for: self)
+    }
+    
+    var sfSymbol: String {
+        switch self {
+            case .camera:
+                "camera"
+            case .microphone:
+                "microphone"
+            case .locationWhenInUse:
+                "location"
+            case .locationAlways:
+                "location.fill"
+            case .notifications:
+                "app.badge"
+            case .photoLibrary:
+                if #available(iOS 16, *) {
+                    "photo.stack"
+                } else {
+                    "photo"
+                }
+            case .contacts:
+                "person.2"
+            case .bluetooth:
+                "dot.radiowaves.left.and.right"
+        }
+    }
+    
+    func request() async {
+        guard self.hasRequiredInfoPlistKey() else {
+            return
+        }
+        switch self {
+            case .camera:
+                _ = await AVCaptureDevice.requestAccess(for: .video)
+                
+            case .microphone:
+                _ = await AVCaptureDevice.requestAccess(for: .audio)
+                
+            case .locationWhenInUse:
+                LocationHelper.shared.requestWhenInUse()
+                
+            case .notifications:
+                _ = try? await UNUserNotificationCenter.current()
+                    .requestAuthorization(options: [.alert, .sound, .badge])
+                
+            case .photoLibrary:
+                _ = await PHPhotoLibrary.requestAuthorization(for: .readWrite)
+                
+            case .locationAlways:
+                LocationHelper.shared.requestAlways()
+                
+            case .contacts:
+                _ = try? await CNContactStore().requestAccess(for: .contacts)
+                
+            case .bluetooth:
+                _ = BluetoothPermissionHelper.shared
+        }
+    }
+    
+    func isAuthorized() async -> Bool {
+        switch self {
+            case .camera:
+                return AVCaptureDevice.authorizationStatus(for: .video) == .authorized
+                
+            case .microphone:
+                return AVCaptureDevice.authorizationStatus(for: .audio) == .authorized
+                
+            case .locationWhenInUse:
+                let status = CLLocationManager.authorizationStatus()
+                return status == .authorizedWhenInUse || status == .authorizedAlways
+                
+            case .locationAlways:
+                let status = CLLocationManager.authorizationStatus()
+                return status == .authorizedAlways
+                
+            case .notifications:
+                let settings = await UNUserNotificationCenter.current().notificationSettings()
+                return settings.authorizationStatus == .authorized || settings.authorizationStatus == .provisional
+                
+            case .photoLibrary:
+                let status = PHPhotoLibrary.authorizationStatus(for: .readWrite)
+                return status == .authorized || status == .limited
+                
+            case .contacts:
+                let status = CNContactStore.authorizationStatus(for: .contacts)
+                return status == .authorized
+                
+            case .bluetooth:
+                return BluetoothPermissionHelper.shared.isAuthorized
+        }
+    }
+        
+    
+
+    func hasRequiredInfoPlistKey() -> Bool {
+        let bundle = Bundle.main
+        
+        guard let key = self.requiredPlistKey else { return true }
+        let hasKey = bundle.object(forInfoDictionaryKey: key) != nil
+        if !hasKey {
+            let logger = Logger(subsystem: "com.timi2506.Permissions", category: "error")
+            logger.critical("Required Info Plist Key Missing for \(self.rawValue): \(self.requiredPlistKey ?? "NONE")")
+            print("CRITICAL: Required Info Plist Key Missing for \(self.rawValue): \(self.requiredPlistKey ?? "NONE") - This incident has been logged")
+        }
+        return hasKey
+    }
+    private var requiredPlistKey: String? {
+        var requiredKey: String?
+        switch self {
+            case .camera:
+                requiredKey = "NSCameraUsageDescription"
+            case .microphone:
+                requiredKey = "NSMicrophoneUsageDescription"
+            case .locationWhenInUse:
+                requiredKey = "NSLocationWhenInUseUsageDescription"
+            case .locationAlways:
+                requiredKey = "NSLocationAlwaysAndWhenInUseUsageDescription"
+            case .photoLibrary:
+                requiredKey = "NSPhotoLibraryUsageDescription"
+            case .contacts:
+                requiredKey = "NSContactsUsageDescription"
+            case .bluetooth:
+                requiredKey = "NSBluetoothAlwaysUsageDescription"
+            case .notifications:
+                requiredKey = nil
+        }
+        return requiredKey
+    }
+}
+
+@MainActor
+public class PermissionsManager: ObservableObject {
+    private init() {}
+    /// The Shared Singleton of PermissionsManager which also allows it to communicate with PermissionsSheet
+    static let shared = PermissionsManager()
+    
+    /// The Localization Provider used to Localize the Permissions' Titles and Descriptions, you can set this to a Custom Provider using setLocalizationProvider, or by modifying this variable, and passing in a struct that conforms to PermissionsLocalizationProvider
+    public var localizationProvider: PermissionsLocalizationProvider = .defaultProvider
+    /// Requests Permissions for the specified Types - Set of Permission Types
+    public func requestPermissions(for types: Set<PermissionType>) async {
+        for type in types {
+            await type.request()
+        }
+    }
+    /// Gets the Permission States for the specified Types with true meaning authorized and false meaning declined.
+    public func getPermissionStates(for types: Set<PermissionType>) async -> [PermissionType: Bool] {
+        var results: [PermissionType: Bool] = [:]
+        for type in types {
+            results[type] = await type.isAuthorized()
+        }
+        return results
+    }
+    
+    public func setLocalizationProvider(to newProvider: PermissionsLocalizationProvider) {
+        self.localizationProvider = newProvider
+    }
+}
+
+public extension PermissionsManager {
+    /// Requests Permissions for the specified Types - Variadic Parameters
+    func requestPermissions(for types: PermissionType...) async {
+        for type in types {
+            await type.request()
+        }
+        var types: Set<PermissionType> = []
+        for type in types {
+            types.insert(type)
+        }
+        await requestPermissions(for: types)
+    }
+    /// Requests Permissions for the specified Type
+    func requestPermissions(for type: PermissionType) async {
+        await requestPermissions(for: [type])
+    }
+    
+    /// Gets the Permission States for the specified Types with true meaning authorized and false meaning declined.
+    func getPermissionStates(for types: PermissionType...) async -> [PermissionType: Bool] {
+        for type in types {
+            await type.request()
+        }
+        var types: Set<PermissionType> = []
+        for type in types {
+            types.insert(type)
+        }
+        return await getPermissionStates(for: types)
+    }
+    /// Requests Permissions for the specified Type
+    func getPermissionState(for type: PermissionType) async -> Bool {
+        return await type.isAuthorized()
+    }
+}
+
+private class LocationHelper: NSObject, CLLocationManagerDelegate {
+    @MainActor static let shared = LocationHelper()
+    private let manager = CLLocationManager()
+    
+    func requestWhenInUse() {
+        manager.requestWhenInUseAuthorization()
+    }
+    func requestAlways() {
+        manager.requestAlwaysAuthorization()
+    }
+}
+
+final private class BluetoothPermissionHelper: NSObject, CBCentralManagerDelegate {
+    @MainActor static let shared = BluetoothPermissionHelper()
+    private var manager: CBCentralManager?
+    private(set) var isAuthorized = false
+    
+    override init() {
+        super.init()
+        manager = CBCentralManager(delegate: self, queue: nil)
+    }
+    
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        switch central.state {
+            case .poweredOn:
+                isAuthorized = true
+            default:
+                isAuthorized = false
+        }
+    }
+}
